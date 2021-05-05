a2a6b60961b5f80b490a6289e8e28f9c89c01dc5
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-05 15:36:59 -0500

    Add Players class

--- bin/main.rb
+++ bin/main.rb
@@ -24,3 +24,12 @@ class Game
     end
   end
 end
+
+class Players < Game
+  attr_reader :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
