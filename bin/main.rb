d34d0ab9823f3473e80e0a2e041e4ce7958dddc0
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-05 15:43:09 -0500

    Fix linters

--- bin/main.rb
+++ bin/main.rb
@@ -4,12 +4,8 @@ class Game
   def start
     system 'clear'
 
-    # Create Global Variable to Tic tac toe's board
-    $board = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9]
-    ]
+    # Create Variable to Tic tac toe's board
+    @game_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
   # Display the Tic tac toe's board
@@ -18,7 +14,7 @@ class Game
       puts ' +---+---+---+'
       (0..7).each do |j|
         print ' | ' if j.even?
-        print $board[i][((j * 3) / 7).to_i] if j.odd?
+        print @game_board[i][((j * 3) / 7).to_i] if j.odd?
       end
       puts i == 2 ? "\n +---+---+---+" : nil
     end
@@ -29,6 +25,7 @@ class Players < Game
   attr_reader :player1, :player2
 
   def initialize(player1, player2)
+    super
     @player1 = player1
     @player2 = player2
   end
