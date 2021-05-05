25f81babdf907b4fde94a8a287177bd536b7c659
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-05 15:36:16 -0500

    Add Game class

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+class Game
+  def start
+    system 'clear'
+
+    # Create Global Variable to Tic tac toe's board
+    $board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+  end
+
+  # Display the Tic tac toe's board
+  def display_board
+    (0..2).each do |i|
+      puts ' +---+---+---+'
+      (0..7).each do |j|
+        print ' | ' if j.even?
+        print $board[i][((j * 3) / 7).to_i] if j.odd?
+      end
+      puts i == 2 ? "\n +---+---+---+" : nil
+    end
+  end
+end
